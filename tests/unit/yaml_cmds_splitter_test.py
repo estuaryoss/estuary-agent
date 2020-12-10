81f78b9e7818593344efca4d86d418552d3a4360
Catalin Dinuta <constantin.dinuta@gmail.com>
2020-12-10 10:02:41 +0200

    added install tags

--- tests/unit/yaml_cmds_splitter_test.py
+++ tests/unit/yaml_cmds_splitter_test.py
@@ -12,6 +12,12 @@ class RenderTestCase(unittest.TestCase):
         yaml_config_string = """
         env:
           MINE: yours
+        before_install:
+          - echo before_install
+        install:
+          - echo install
+        after_install:
+          - echo after_install
         before_script:
           - echo before_script
         script:
@@ -20,7 +26,29 @@ class RenderTestCase(unittest.TestCase):
           - echo after_script
         """
         splitter = YamlCommandsSplitter(yaml.safe_load(yaml_config_string))
-        self.assertEqual(len(splitter.get_cmds_in_order()), 3)
+        self.assertEqual(len(splitter.get_cmds_in_order()), 6)
+
+    def test_full_yml_skipped_key(self):
+        yaml_config_string = """
+        env:
+          MINE: yours
+        this_key_is_skipped:
+          - echo skipped
+        before_install:
+          - echo before_install
+        install:
+          - echo install
+        after_install:
+          - echo after_install
+        before_script:
+          - echo before_script
+        script:
+          - echo script
+        after_script:
+          - echo after_script
+        """
+        splitter = YamlCommandsSplitter(yaml.safe_load(yaml_config_string))
+        self.assertEqual(len(splitter.get_cmds_in_order()), 6)
 
     def test_missing_before_yml(self):
         yaml_config_string = """
